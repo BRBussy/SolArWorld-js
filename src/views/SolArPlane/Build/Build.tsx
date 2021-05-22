ee35a592bebb543dfa82e4a03b58a5896af1e77f
Bernard Bussy <brbitzbussy@gmail.com>
2021-05-22 21:24:07 +0200

    starto

--- src/views/SolArPlane/Build/Build.tsx
+++ src/views/SolArPlane/Build/Build.tsx
@@ -1,15 +1,45 @@
 import React from "react";
 import {
-    makeStyles,
+    Card, CardContent,
+    makeStyles, TextField,
 } from "@material-ui/core";
 
-const useStyles = makeStyles((theme) => ({}))
+const useStyles = makeStyles((theme) => ({
+    field: {
+        minWidth: 500
+    }
+}))
 
 export default function Build() {
     const classes = useStyles();
+
     return (
-        <div>
-            build
-        </div>
+        <Card>
+            <CardContent>
+                <TextField
+                    className={classes.field}
+                    label={'Positive Land Program Address'}
+                    placeholder={'e.g. 6TkKqq15wXjqEjNg9zqTKADwuVATR9dW3rkNnsYme1ea'}
+                />
+                <TextField
+                    className={classes.field}
+                    label={'PLP NFT Owner (SPL Token Acc. Owner)'}
+                    placeholder={'e.g. 6TkKqq15wXjqEjNg9zqTKADwuVATR9dW3rkNnsYme1ea'}
+                />
+                <TextField
+                    className={classes.field}
+                    label={'X Location'}
+                />
+                <TextField
+                    className={classes.field}
+                    label={'Y Location'}
+                />
+                <TextField
+                    className={classes.field}
+                    label={'PLP_X_Y Decorator Acc.'}
+                    placeholder={'e.g. 6TkKqq15wXjqEjNg9zqTKADwuVATR9dW3rkNnsYme1ea'}
+                />
+            </CardContent>
+        </Card>
     )
 }
