7f021bab6bd80f554c99a0ea94f9fb423fd9f3ef
Bernard Bussy <brbitzbussy@gmail.com>
2021-05-22 21:14:47 +0200

    render solarplane

--- src/route/routes.ts
+++ src/route/routes.ts
@@ -2,11 +2,13 @@ import RouteType from './Route';
 import {
     AccountBalanceWallet as WalletIcon,
     Storage as ARWeaveIcon,
-    Settings as SolanaIcon
+    Settings as SolanaIcon,
+    MapSharp as SolArPlaneIcon
 } from '@material-ui/icons';
 import Wallet from '../views/Wallet';
 import Arweave from '../views/Arweave';
 import Solana from '../views/Solana';
+import SolArPlane from '../views/SolArPlane';
 
 export const defaultRoute: RouteType = {
     name: 'Wallet',
@@ -32,6 +34,14 @@ export const publicRoutes: RouteType[] = [
         path: '/solana',
         component: Solana,
         icon: SolanaIcon,
+        allowSubPaths: true
+    },
+    {
+        name: 'SolArPlane',
+        id: 'solArPlane',
+        path: '/solArPlane',
+        component: SolArPlane,
+        icon: SolArPlaneIcon,
         allowSubPaths: true
     },
 ]
