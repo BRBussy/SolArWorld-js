abb11bf43d9072d1aebeedd7925b18721de3544d
Bernard Bussy <brbitzbussy@gmail.com>
2021-05-23 20:08:18 +0200

    boxes are a spinning

--- src/views/SolArWorld/SpinningBox.tsx
+++ src/views/SolArWorld/SpinningBox.tsx
@@ -57,30 +57,30 @@ const SpinningBox = (props: SpinningBoxProps) => {
 }
 
 export const SceneWithSpinningBoxes = () => (
-    <div>
-        <Engine antialias adaptToDeviceRatio canvasId='babylonJS'>
-            <Scene>
-                <arcRotateCamera
-                    name="camera1"
-                    target={Vector3.Zero()}
-                    alpha={Math.PI / 2}
-                    beta={Math.PI / 4}
-                    radius={8}
-                />
-                <hemisphericLight name='light1' intensity={0.7} direction={Vector3.Up()}/>
-                <SpinningBox
-                    name={'left'}
-                    position={new Vector3(-2, 0, 0)}
-                    color={Color3.FromHexString('#EEB5EB')}
-                    hoveredColor={Color3.FromHexString('#C26DBC')}
-                />
-                <SpinningBox
-                    name='right'
-                    position={new Vector3(2, 0, 0)}
-                    color={Color3.FromHexString('#C8F4F9')}
-                    hoveredColor={Color3.FromHexString('#3CACAE')}
-                />
-            </Scene>
-        </Engine>
-    </div>
+    <Engine
+        antialias canvasId='babylonJS'
+    >
+        <Scene>
+            <arcRotateCamera
+                name="camera1"
+                target={Vector3.Zero()}
+                alpha={Math.PI / 2}
+                beta={Math.PI / 4}
+                radius={10}
+            />
+            <hemisphericLight name='light1' intensity={0.7} direction={Vector3.Up()}/>
+            <SpinningBox
+                name={'left'}
+                position={new Vector3(-2, 0, 0)}
+                color={Color3.FromHexString('#EEB5EB')}
+                hoveredColor={Color3.FromHexString('#C26DBC')}
+            />
+            <SpinningBox
+                name='right'
+                position={new Vector3(2, 0, 0)}
+                color={Color3.FromHexString('#C8F4F9')}
+                hoveredColor={Color3.FromHexString('#3CACAE')}
+            />
+        </Scene>
+    </Engine>
 )
