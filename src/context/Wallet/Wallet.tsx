824ff71127bfddb3c0017bc8148e4c1b6c41c888
Bernard Bussy <brbitzbussy@gmail.com>
2021-06-04 20:34:53 +0200

    installem!

--- src/context/Wallet/Wallet.tsx
+++ src/context/Wallet/Wallet.tsx
@@ -1,77 +1,24 @@
-import React, {useContext, useLayoutEffect, useState} from 'react';
-import {SolanaKey} from "../../solArWorld/solana";
-import {Keypair} from "@solana/web3.js";
-import Wallet from "../../solArWorld/solana/wallet";
-
-interface WalletWithDetails {
-    id: string;
-    wallet: Wallet;
-    name: 'string';
-}
+import React, {useContext, useRef, useState} from 'react';
+import Wallet, {PhantomWallet} from "../../solArWorld/solana/wallet";
 
 interface ContextType {
-    wallets: WalletWithDetails[];
+    wallets: Wallet[];
+    selectedWallet: Wallet;
 }
 
 const Context = React.createContext({} as ContextType);
 
-const localStorageWalletDataKey = 'wallet-savedWallet';
-
 function WalletContext({children}: { children?: React.ReactNode }) {
-    const [wallet, setWallet] = useState<Wallet>(new Wallet());
-
-    //
-    // Load any saved wallet data on first run
-    //
-    useLayoutEffect(() => {
-        // look for saved wallet data in local storage
-        const savedWalletJSONString = localStorage.getItem(localStorageWalletDataKey);
-        if (!savedWalletJSONString) {
-            // if no data is found then do nothing
-            return;
-        }
-
-        // saved wallet data exists in local storage ---> try to parse the data
-        try {
-            setWallet(new Wallet(JSON.parse(
-                savedWalletJSONString,
-                (name, value) => {
-                    if (name === 'solanaKeys') {
-                        const solanaKeys = value as {
-                            description: string,
-                            solanaKeyPair: {
-                                keypair: {
-                                    publicKey: { [key: number]: number },
-                                    secretKey: { [key: number]: number },
-                                }
-                            }
-                        }[];
-                        return solanaKeys.map((k, idx) => new SolanaKey({
-                            description: k.description,
-                            solanaKeyPair: Keypair.fromSecretKey(Uint8Array.from(Object.values(k.solanaKeyPair.keypair.secretKey)))
-                        }))
-                    }
-                    return value;
-                }
-            )))
-        } catch (e) {
-            // if there was any error during parsing then clear the local storage
-            console.error(`error parsing wallet from local storage: ${e.toString()}`);
-            localStorage.removeItem(localStorageWalletDataKey);
-        }
-    }, [])
-
-    // to update the wallet stored in state (and local storage)
-    const handleSetWallet = (updatedWallet: Wallet) => {
-        localStorage.setItem(localStorageWalletDataKey, JSON.stringify(updatedWallet));
-        setWallet(updatedWallet);
-    }
+    const {current: wallets} = useRef([
+        PhantomWallet
+    ]);
+    const [selectedWallet, setSelectedWallet] = useState<Wallet>(wallets[0]);
 
     return (
         <Context.Provider
             value={{
-                wallet,
-                setWallet: handleSetWallet
+                wallets,
+                selectedWallet
             }}
         >
             {children}
