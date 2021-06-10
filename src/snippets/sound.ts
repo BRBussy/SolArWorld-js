ffb94b6c9fafb3cb8a2dc9c63ef0e3462416086d
Bernard Bussy <brbitzbussy@gmail.com>
2021-06-10 19:51:45 +0200

    soundies

---
+++ src/snippets/sound.ts
@@ -0,0 +1,25 @@
+// import {downloadBlob} from "../utilities/network";
+// import {Sound} from "@babylonjs/core";
+//
+// const blob = await downloadBlob(
+//     'https://storage.googleapis.com/sol-ar-world/cello.mp3'
+// );
+//
+// let sound: Sound;
+// sound = new Sound(
+//     'Cello',
+//     await blob.arrayBuffer(),
+//     scene,
+//     () => {
+//         if (!sound.isReady()) {
+//             console.error('sound is not ready to play');
+//             return;
+//         }
+//         try {
+//             console.log('playing sound');
+//             sound.play();
+//         } catch (e) {
+//             console.error(`error playing sound: ${e}`)
+//         }
+//     },
+// );
