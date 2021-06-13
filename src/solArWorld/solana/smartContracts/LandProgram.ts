29330544eed1c268cdb5b04430bcb64fad9742a9
Bernard Bussy <brbitzbussy@gmail.com>
2021-06-13 14:07:59 +0200

    update

--- src/solArWorld/solana/smartContracts/LandProgram.ts
+++ src/solArWorld/solana/smartContracts/LandProgram.ts
@@ -1,22 +1,13 @@
 import {PublicKey, SYSVAR_RENT_PUBKEY, TransactionInstruction} from "@solana/web3.js";
 import BN from "bn.js";
-import {QuadrantNo, quadrantNumberToLandProgramKey} from "../../genesisRegion";
-
-export type InitialiseLandPlaneAccountParams = {
-    landProgramID: PublicKey;
-    ///   0. `[writable]` The Land Plane account to initialize (Owned by Land program)
-    landPlaneAccountToInitialise: PublicKey;
-}
 
 export type MintParams = {
-    // new land asset account that is the PDA of:
-    // ([
-    //      'land',
-    //      nextXValue,
-    //      nextYValue,
-    // ], landProgramAccPublicKey)
-    // Req: [writable]
-    landAssetAccountPublicKey: PublicKey;
+    // The person that will own the minted land piece.
+    // Signature required to prove that the person who
+    // owns the NFT token atm. is wanting to try do this
+    // mint.
+    // Req: [signer]
+    newNFTTokenAccOwnerAccPubKey: PublicKey;
 
     // spl associated token holding acc for nft
     // i.e. PDA of:
@@ -27,18 +18,25 @@ export type MintParams = {
     // Req: [signer]
     newNFTOwnerAccAssociatedTokenAccPublicKey: PublicKey
 
-    // the person that will own the minted land piece
-    // Req: [signer]
-    newNFTTokenAccOwnerAccPubKey: PublicKey;
+    // new land asset account that is the PDA of:
+    // ([
+    //      'land',
+    //      nextXValue,
+    //      nextYValue,
+    // ], landProgramAccPublicKey)
+    // Req: [writable]
+    landAssetAccountPublicKey: PublicKey;
 
     // spl mint acc for nft
     // Req: []
     nftMintAccPubKey: PublicKey;
+
+    // public key of the land contract to perform mint with
+    // Req: []
+    landProgramAccPublicKey: PublicKey;
 }
 
 export const LAND_PLANE_ACC_SIZE = 10
-
-export const MAX_NO_LAND_PIECES = 10
 
 export interface LandProgramSmartContract {
     mint(params: MintParams): TransactionInstruction;
@@ -47,7 +45,7 @@ export interface LandProgramSmartContract {
 export const LandProgram: LandProgramSmartContract = {
     mint(params: MintParams): TransactionInstruction {
         return new TransactionInstruction({
-            programId: quadrantNumberToLandProgramKey(params.quadrantNo),
+            programId: params.landProgramAccPublicKey,
             keys: [
                 // 1st
                 // Addresses requiring signatures are 1st, and in the following order:
@@ -62,7 +60,7 @@ export const LandProgram: LandProgramSmartContract = {
                 // those that require write access
                 // those that require read-only access
             ],
-            data: Buffer.from(Uint8Array.of(0, ...new BN(1).toArray("le", 8)))
+            data: Buffer.from(Uint8Array.of(0, ...new BN(0).toArray("le", 8)))
         })
     }
 }
