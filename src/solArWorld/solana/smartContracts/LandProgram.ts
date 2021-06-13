65cb31674f5b1163c5ff1b5423ae886b85af7829
Bernard Bussy <brbitzbussy@gmail.com>
2021-06-13 13:59:35 +0200

    start updating mint params

--- src/solArWorld/solana/smartContracts/LandProgram.ts
+++ src/solArWorld/solana/smartContracts/LandProgram.ts
@@ -8,10 +8,32 @@ export type InitialiseLandPlaneAccountParams = {
     landPlaneAccountToInitialise: PublicKey;
 }
 
-export type MintLandPiecesParams = {
-    quadrantNo: QuadrantNo;
-    nftTokenAccOwnerAccPubKey: PublicKey;
-    noOfPiecesToMint: number;
+export type MintParams = {
+    // new land asset account that is the PDA of:
+    // ([
+    //      'land',
+    //      nextXValue,
+    //      nextYValue,
+    // ], landProgramAccPublicKey)
+    // Req: [writable]
+    landAssetAccountPublicKey: PublicKey;
+
+    // spl associated token holding acc for nft
+    // i.e. PDA of:
+    // ([
+    //      newNFTOwnerPublicKey, tokenProgramPubKey, nftMintAccountPubKey
+    //  ], splAssociatedTokenAccountProgramID)
+    // Note: must hold a balance of 1 of this
+    // Req: [signer]
+    newNFTOwnerAccAssociatedTokenAccPublicKey: PublicKey
+
+    // the person that will own the minted land piece
+    // Req: [signer]
+    newNFTTokenAccOwnerAccPubKey: PublicKey;
+
+    // spl mint acc for nft
+    // Req: []
+    nftMintAccPubKey: PublicKey;
 }
 
 export const LAND_PLANE_ACC_SIZE = 10
@@ -19,34 +41,11 @@ export const LAND_PLANE_ACC_SIZE = 10
 export const MAX_NO_LAND_PIECES = 10
 
 export interface LandProgramSmartContract {
-    initialiseLandPlaneAccount(params: InitialiseLandPlaneAccountParams): TransactionInstruction;
-
-    mintLandPieces(params: MintLandPiecesParams): TransactionInstruction;
+    mint(params: MintParams): TransactionInstruction;
 }
 
 export const LandProgram: LandProgramSmartContract = {
-    initialiseLandPlaneAccount(params: InitialiseLandPlaneAccountParams): TransactionInstruction {
-        return new TransactionInstruction({
-            programId: params.landProgramID,
-            keys: [
-                // 1st
-                // Addresses requiring signatures are 1st, and in the following order:
-                //
-                // those that require write access
-                // those that require read-only access
-
-                // 2nd
-                // Addresses not requiring signatures are 2nd, and in the following order:
-                //
-                // those that require write access
-                {pubkey: params.landPlaneAccountToInitialise, isSigner: false, isWritable: true},
-                // those that require read-only access
-                {pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false},
-            ],
-            data: Buffer.from(Uint8Array.from([0]))
-        })
-    },
-    mintLandPieces(params: MintLandPiecesParams): TransactionInstruction {
+    mint(params: MintParams): TransactionInstruction {
         return new TransactionInstruction({
             programId: quadrantNumberToLandProgramKey(params.quadrantNo),
             keys: [
@@ -55,7 +54,7 @@ export const LandProgram: LandProgramSmartContract = {
                 //
                 // those that require write access
                 // those that require read-only access
-                {pubkey: params.nftTokenAccOwnerAccPubKey, isSigner: true, isWritable: false},
+                {pubkey: params.newNFTTokenAccOwnerAccPubKey, isSigner: true, isWritable: false},
 
                 // 2nd
                 // Addresses not requiring signatures are 2nd, and in the following order:
